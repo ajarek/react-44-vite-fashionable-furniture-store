61c7ed9abf9363bde92b8850f192ba3d15b8234c
Ajarek <ajarek@poczta.onet.pl>
2022-12-08 00:32:00 +0100

    bug fix filter

--- src/components/CardProducts/CardProducts.jsx
+++ src/components/CardProducts/CardProducts.jsx
@@ -1,33 +1,38 @@
 import { NavLink } from 'react-router-dom'
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './CardProducts.css'
-export const CardProducts = ({length,width,sortData,filterData}) => {
-  const [isData, setIsData]= useState([])
-  useEffect(()=>{
-  fetch('/data.json')
-  .then(res => res.json())
-  .then(res => {
-    setIsData(res);
+export const CardProducts = ({ length, width, sortData, filterData }) => {
+  const [isData, setIsData] = useState([])
+  
+  useEffect(() => {
+    fetch('/data.json')
+      .then((res) => res.json())
+      .then((res) => {
+        setIsData(res.filter(filterData))
+      })
   })
- 
-})
+  
   return (
-    <div className="wrapper-featured">
-    {isData?.filter(filterData).sort(sortData).map((el)=>{
-       if(el.id<=length)
-      return(
-       <div key={el.id}>
-         <NavLink
+    <div className='wrapper-featured'>
+      {isData?.sort(sortData).map((el, index, arr) => {
+        if (el.id <= length)
+          return (
+            <div key={el.id}>
+              <NavLink
                 className={'button-link'}
                 to={`/product/${el.id}`}
               >
-       <img src={'/images/'+el.src} alt={el.name} width={width} />
-      <p>{el.name}</p>
-      <p>{'$'+el.price}</p>
-      </NavLink>
-      </div>
-      )
-    })}
+                <img
+                  src={'/images/' + el.src}
+                  alt={el.name}
+                  width={width}
+                />
+                <p>{el.name}</p>
+                <p>{'$' + el.price}</p>
+              </NavLink>
+            </div>
+          )
+      })}
     </div>
   )
 }
